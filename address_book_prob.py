845a77c91de088da57299eb3f9c5ccea2d4501ab
saloni1108 <saloni1108goyal@gmail.com>
2024-04-27 12:52:07 +0530

    [Saloni] Refactor: create a Contacts in Address Book with first and last names, address, city, state, zip, phone number and email

--- address_book_prob.py
+++ address_book_prob.py
@@ -10,11 +10,22 @@ logging.basicConfig(filename="address_book.log", filemode="a", level=logging.INF
 
 logger = logging.getLogger(__name__)
 
-def add_contact(first_name, last_name, address, city, state, zip_code, phone_number, email):
-    return "Name: {} {}\nAddress: {}, {}, {}, {}\nPhone Number: {}\nE-mail: {}".format(
-        first_name, last_name, address, city, state, zip_code, phone_number, email)
 
-def main():
+class Contact:
+    def __init__(self, first_name, last_name, address, city, state, zip_code, phone_number, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.phone_number = phone_number
+        self.email = email
+    
+    def __str__(self):
+        return "Name: {self.first_name} {self.last_name}\nAddress: {self.address}, {self.city}, {self.state}, {self.zip_code}\nPhone Number: {self.phone_number}\nE-mail: {self.email}"
+
+def add_contact(): 
     logger.info("ADDRESS BOOK STARTED...")
     first_name = input("Enter the first name: ")
     last_name = input("Enter the last name: ")
@@ -24,11 +35,15 @@ def main():
     zip_code = input("Enter the postal code: ")
     phone_number = input("Enter a 10-digit phone number: ")
     email = input("Enter an email id: ")
+
+    contact_1 = Contact(first_name, last_name, address, city, state, zip_code, phone_number, email)
+    logger.info("New Contact Added: ", contact_1)
+
+
+def main():
     try:
-        if all(map(str.isalpha, [first_name, last_name, city, state])) and phone_number.isdigit() and zip_code.isdigit():
-            add_contact(first_name, last_name, address, city, state, zip_code, phone_number, email)
-        else:
-            raise Exception("Invalid Input...")
+        logger.info("Address Book Started...")
+        add_contact()
     except Exception as e:
         logger.error("An Error Occured: %s", e)
 
